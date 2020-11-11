ad1f1fb18e1f1f8578817036b3459f56a7c01440
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-12 05:47:18 +0800

    ✨ Added new component generator

---
+++ internals/component.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+const fileNameToComponentName = (fileName) => fileName
+  .split('-')
+  .map((name) => name[0].toUpperCase() + name.substr(1))
+  .join('');
+
+const jsContent = (fileName) => `const ${fileNameToComponentName(fileName)} = () => (
+  <p>new component!</p>
+);
+
+export default ${fileNameToComponentName(fileName)};
+`;
+
+const scssContent = (fileName) => `.${fileName} {
+  background-color: papayawhip;
+}
+`;
+
+const scssImport = (fileName) => `@import 'components/${fileName}/${fileName}';`;
+
+const component = () => {
+  const componentName = process.argv.slice(2)[0] || 'new-component';
+  const componentFileName = componentName.toLowerCase();
+  const componentDirectory = `components/${componentFileName}`;
+  const componentPath = `${componentDirectory}/${componentFileName}`;
+
+  if (!componentName) {
+    console.log('Invalid component name!');
+    return;
+  }
+
+  fs.mkdirSync(componentDirectory);
+
+  fs.appendFile(`${componentPath}.scss`, scssContent(componentFileName), (err) => {
+    if (err) throw err;
+    console.log(`💅 SCSS: Created ${componentFileName}.scss file.`);
+  });
+
+  fs.appendFile('scss/main.scss', scssImport(componentFileName), (err) => {
+    if (err) throw err;
+    console.log(`💅 SCSS: Auto-import ${componentFileName}.scss file.`);
+  });
+
+  fs.appendFile(`${componentPath}.js`, jsContent(componentFileName), (err) => {
+    if (err) throw err;
+    console.log(`⚛️  JS: Created ${componentFileName}.js file.`);
+  });
+
+  console.log('WOOT! ✨');
+};
+
+// init
+component();
