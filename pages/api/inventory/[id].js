0e12240baf2e7f776e19c9b8ebcc94a6dc73fdff
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-12 04:46:36 +0800

    ✨ Added inventory/[id] api | GET | PUT | DELETE

---
+++ pages/api/inventory/[id].js
@@ -0,0 +1,55 @@
+import { PrismaClient } from '@prisma/client';
+import api from 'js/utils/api';
+
+const prisma = new PrismaClient();
+
+export default api({
+  get: async (req, res) => {
+    try {
+      const item = await prisma.inventory.findOne({
+        where: {
+          id: req.query.id,
+        },
+        include: {
+          supplier: true,
+          applications: true,
+          brand: true,
+          uom: true,
+          codes: true,
+        },
+      });
+
+      res.success(item);
+    } catch (error) {
+      res.error(error);
+    }
+  },
+  put: async (req, res) => {
+    try {
+      const updatedItem = await prisma.inventory.update({
+        where: {
+          id: req.query.id,
+        },
+        data: req.body,
+      });
+
+      res.success(updatedItem);
+    } catch (error) {
+      res.error(error);
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const deletedItem = await prisma.inventory.delete({
+        where: {
+          id: req.query.id,
+        },
+      });
+
+      res.success(deletedItem);
+    } catch (error) {
+      console.log(error);
+      res.error(error);
+    }
+  },
+});
