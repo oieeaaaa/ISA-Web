edeb1e789948b6d5a282cd474f2dadc1f14296fe
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-12 12:29:26 +0800

    🧪 Testing out these new components

--- pages/inventory/add.js
+++ pages/inventory/add.js
@@ -1,10 +1,16 @@
 import Layout from 'components/layout/layout';
+import FormTitle from 'components/form-title/form-title';
+import FormSection from 'components/form-section/form-section';
+import InputGroup from 'components/input-group/input-group';
 
 const InventoryAdd = () => (
   <Layout>
     <div className="inventory-add">
-      <div className="grid">
-        Inventory Add
+      <div className="inventory-add-container grid">
+        <FormTitle icon="archive" title="Add Item" />
+        <FormSection title="References">
+          <InputGroup name="number" label="Number" />
+        </FormSection>
       </div>
     </div>
   </Layout>
