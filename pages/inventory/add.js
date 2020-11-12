11caf00a963d21b6849ccf70f7fbd3d8c1146d5d
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-12 09:47:37 +0800

    ✨ Initialized inventory page

---
+++ pages/inventory/add.js
@@ -0,0 +1,13 @@
+import Layout from 'components/layout/layout';
+
+const InventoryAdd = () => (
+  <Layout>
+    <div className="inventory-add">
+      <div className="grid">
+        Inventory Add
+      </div>
+    </div>
+  </Layout>
+);
+
+export default InventoryAdd;
