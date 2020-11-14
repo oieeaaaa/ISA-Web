a67941da43ab5f585b8e4db4f07131cc2590c612
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-14 20:41:14 +0800

    💅 Added className modifiers

--- components/form-section/form-section.js
+++ components/form-section/form-section.js
@@ -1,7 +1,8 @@
+import toKebabCase from 'js/utils/toKebabCase';
 import FormSectionTitle from 'components/form-section-title/form-section-title';
 
 const FormSection = ({ title, children }) => (
-  <div className="form-section">
+  <div className={`form-section form-section--${toKebabCase(title)}`}>
     <FormSectionTitle title={title} />
     {children}
   </div>
--- components/input-group/input-group.js
+++ components/input-group/input-group.js
@@ -4,7 +4,7 @@ const InputGroup = ({
   component: Component,
   ...etc
 }) => (
-  <div className="input-group">
+  <div className={`input-group input-group--${name}`}>
     <label className="input-group__label" htmlFor={name}>
       {label}
     </label>
