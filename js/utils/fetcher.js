85d78a5ce6a92e08658b60352d25076a41fe4b65
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-17 00:03:12 +0800

    ✨ ADDED NEW UTILS AND APIS

---
+++ js/utils/fetcher.js
@@ -0,0 +1,26 @@
+/**
+ * fetcher.
+ * An extended version of fetch
+ * Built by yours truly: Joimee T. Cajandab
+ *
+ * @param {string} route
+ * @param {object} options
+ */
+const fetcher = async (route, options = {}) => {
+  const { NEXT_PUBLIC_API_URL } = process.env;
+
+  try {
+    const rawResult = await fetch(`${NEXT_PUBLIC_API_URL}/api${route}`, {
+      method: 'GET',
+      ...options,
+    });
+
+    const result = await rawResult.json();
+
+    return result;
+  } catch (err) {
+    return err;
+  }
+};
+
+export default fetcher;
