33e194d9097b93baf942d77889ca495835a7e3dd
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-18 20:58:46 +0800

    🏗 Handled error

--- components/input-group/input-group.js
+++ components/input-group/input-group.js
@@ -1,15 +1,25 @@
+import { useFormikContext } from 'formik';
+
 const InputGroup = ({
   name,
   label,
   component: Component,
+  error,
   ...etc
-}) => (
-  <div className={`input-group input-group--${name}`}>
-    <label className="input-group__label" htmlFor={name}>
-      {label}
-    </label>
-    <Component id={name} name={name} {...etc} />
-  </div>
-);
+}) => {
+  const { errors } = useFormikContext();
+
+  return (
+    <div className={`input-group input-group--${name}`}>
+      <label className="input-group__label" htmlFor={name}>
+        {label}
+      </label>
+      <Component id={name} name={name} {...etc} />
+      {errors[name] && (
+        <p className="input-group__error">{error || errors[name]}</p>
+      )}
+    </div>
+  );
+};
 
 export default InputGroup;
