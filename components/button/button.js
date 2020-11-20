37f0957bb4adceda774d641959421e849c744868
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-20 10:10:03 +0800

    ✨ Enhanced button: Icon support

--- components/button/button.js
+++ components/button/button.js
@@ -1,15 +1,28 @@
+import joinClassName from 'js/utils/joinClassName';
+import Icon from 'components/icon/icon';
+
 /**
  * Button.
  *
  * @param {object} props
-   * @param {string} variant | default | primary
+   * @param {string} variant | default | primary | primary-v1
  */
-const Button = ({ variant = 'default', children, ...etc }) => (
+// TODO: Create a function that contatenate classNames if the corresponding conditions are true
+const Button = ({
+  variant = 'default',
+  icon = '',
+  children,
+  className,
+  ...etc
+}) => (
   <button
-    className={`button button--${variant}`}
+    className={joinClassName(`button button--${variant} ${icon ? 'button--with-icon' : ''}`, className)}
     type="button"
     {...etc}
   >
+    {icon && (
+      <Icon icon={icon} />
+    )}
     {children}
   </button>
 );
