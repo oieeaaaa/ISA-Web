62ddcf2afa2315206b1d6d5d996af9408b7429b6
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-20 22:13:27 +0800

    ✨ Added: TableWithFetch

---
+++ components/table-with-fetch/table-with-fetch.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import fetcher from 'js/utils/fetcher';
+import toParam from 'js/utils/toParam';
+import Table from 'components/table/table';
+
+const TableWithFetch = ({
+  serverRoute,
+  parameterizer, // cool name
+  data,
+  ...tableProps
+}) => {
+  const [tableData, setTableData] = useState(data);
+
+  const handleFetch = async ({
+    limit,
+    sortBy,
+    direction,
+    search,
+    page,
+    ...configs
+  }) => {
+    try {
+      const param = toParam({
+        direction,
+        search,
+        page,
+        limit: limit.value,
+        sortBy: sortBy.key,
+        ...parameterizer(configs),
+      });
+
+      const url = `/${serverRoute}?${param}`;
+      const result = await fetcher(url);
+
+      setTableData(result.data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <Table
+      title="Inventory"
+      icon="archive"
+      data={tableData}
+      onChange={handleFetch}
+      {...tableProps}
+    />
+  );
+};
+
+export default TableWithFetch;
