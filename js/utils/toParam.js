03edff62d5fb7e9d68fbade2ca5b435c8fc714b9
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-20 22:13:39 +0800

    ✨ Added: toParam (util)

---
+++ js/utils/toParam.js
@@ -0,0 +1,25 @@
+/**
+ * toParam.
+ * From object to query params
+ * @param {object} param
+ *
+ * Sample usage:
+ ```
+  const myParam = {
+    search: 'amazinggg',
+    limit: 5,
+    page: 2
+  };
+
+  // convert to query params
+  toParam(myParam); // that's it.
+
+  // output: search="amazinggg"&limit=5&page=2
+ ```
+ */
+const toParam = (param = {}) => Object
+  .entries(param) // split key/value and store it to array
+  .filter(([, value]) => !!value) // omit falsy values
+  .map(([key, value]) => `${key}=${value}`).join('&'); // actual param
+
+export default toParam;
