5ecd077d7a7703564fa06a11ff074b7d0dbeb54d
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-20 22:16:27 +0800

    🏗  Refactored: Use TableWithFetch

--- pages/inventory/index.js
+++ pages/inventory/index.js
@@ -1,74 +1,52 @@
 import fetcher from 'js/utils/fetcher';
+import {
+  tableHeaders,
+  tableFilters,
+  tableSortOptions,
+} from 'js/shapes/inventory';
 import Layout from 'components/layout/layout';
-import Table from 'components/table/table';
+import TableWithFetch from 'components/table-with-fetch/table-with-fetch';
 import InputGroup from 'components/input-group/input-group';
 import Select from 'components/select/select';
 
-const Inventory = ({ data, helpers }) => (
-  <Layout>
-    <Table
-      title="Inventory"
-      icon="archive"
-      headers={[{
-        label: 'Reference No.',
-        accessKey: 'referenceNumber',
-      }, {
-        label: 'Particular',
-        accessKey: 'particular',
-      }, {
-        label: 'Codes',
-        accessKey: 'codes',
-      }, {
-        label: 'Brand',
-        accessKey: 'brand.name',
-      }, {
-        label: 'Supplier',
-        accessKey: 'supplier.name',
-      }, {
-        label: 'Applications',
-        accessKey: 'applications',
-        customCell: ({ value }) => {
-          const applicationNames = value.map((val) => val.name).join(', ');
+const Inventory = ({ data, helpers }) => {
+  const parameterizer = (params) => ({
+    brand: params.brand.id,
+    owner: params.owner.id,
+  });
 
-          return <p>{applicationNames}</p>;
-        },
-      }]}
-      data={data}
-      filters={{
-        owner: {},
-        brand: {},
-      }}
-      sortOptions={[
-        {
-          id: 1,
-          name: 'Reference No.',
-        },
-        {
-          id: 2,
-          name: 'Particular',
-        },
-      ]}
-      renderFilter={() => (
-        <div className="inventory-filters">
-          <InputGroup
-            name="owner"
-            label="Owner"
-            component={Select}
-            options={[]}
-          />
-          <InputGroup
-            name="brand"
-            label="Brand"
-            component={Select}
-            options={helpers.brands}
-            mainKey="name"
-          />
-        </div>
-      )}
-      onChange={console.log}
-    />
-  </Layout>
-);
+  return (
+    <Layout>
+      <TableWithFetch
+        serverRoute="inventory"
+        parameterizer={parameterizer}
+        title="Inventory"
+        icon="archive"
+        headers={tableHeaders}
+        data={data}
+        filters={tableFilters}
+        sortOptions={tableSortOptions}
+        renderFilter={() => (
+          <div className="inventory-filters">
+            <InputGroup
+              name="owner"
+              label="Owner"
+              component={Select}
+              options={[]}
+            />
+            <InputGroup
+              name="brand"
+              label="Brand"
+              component={Select}
+              options={helpers.brands}
+              mainKey="name"
+            />
+          </div>
+        )}
+      />
+    </Layout>
+  );
+};
 
 export async function getStaticProps() {
   const { data } = await fetcher('/inventory');
