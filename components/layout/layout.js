96747a6c6245a08a88f32fbbbe089eaa5127cd7e
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-21 17:42:08 +0800

    💅 Desktop: Layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -6,11 +6,13 @@ import Footer from 'components/footer/footer';
 
 const Layout = ({ children }) => (
   <LayoutProvider>
-    <Header />
-    <Sidebar />
-    <div className="main-content">{children}</div>
-    <Footer />
-    <GridGuide />
+    <div className="layout">
+      <Header />
+      <Sidebar />
+      <div className="main-content">{children}</div>
+      <Footer />
+      <GridGuide />
+    </div>
   </LayoutProvider>
 );
 
