d715c458c7c8858d98a1cb963450b813ff9558aa
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-23 08:57:28 +0800

    💅 Desktop: FormSection

--- components/form-section/form-section.js
+++ components/form-section/form-section.js
@@ -4,7 +4,7 @@ import FormSectionTitle from 'components/form-section-title/form-section-title';
 const FormSection = ({ title, children }) => (
   <div className={`form-section form-section--${toKebabCase(title)}`}>
     <FormSectionTitle title={title} />
-    {children}
+    <div className="form-section__content">{children}</div>
   </div>
 );
 
