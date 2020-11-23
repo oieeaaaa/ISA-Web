bf174179b174658558f77f7cc28c3edba4ce98b9
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-23 21:25:24 +0800

    🏗  Restructured commonGetHelper

--- js/utils/commonGetHelper.js
+++ js/utils/commonGetHelper.js
@@ -1,21 +1,28 @@
 import { PrismaClient } from '@prisma/client';
+import isObjectEmpty from 'js/utils/isObjectEmpty';
+import toFullTextSearchQuery from 'js/utils/toFullTextSearchQuery';
 
 const prisma = new PrismaClient();
 
 export default (table = '', otherProps = {}) => async (req, res) => {
   try {
     const { search = '', page = 1, limit = 5 } = req.query;
+    const isOtherPropsEmpty = isObjectEmpty(otherProps);
 
     const items = await prisma[table].findMany({
-      select: {
-        id: true,
-        name: true,
-        ...otherProps
-      },
+      select: isOtherPropsEmpty
+        ? {
+            id: true,
+            name: true
+          }
+        : {
+            id: true,
+            ...otherProps
+          },
       where: {
-        name: {
-          contains: search
-        }
+        OR: isOtherPropsEmpty
+          ? toFullTextSearchQuery(['name'], search)
+          : toFullTextSearchQuery(Object.keys(otherProps), search)
       },
       skip: (page - 1) * limit,
       take: limit
--- pages/api/helpers/supplier.js
+++ pages/api/helpers/supplier.js
@@ -2,5 +2,8 @@ import api from 'js/utils/api';
 import commonGetHelper from 'js/utils/commonGetHelper';
 
 export default api({
-  get: commonGetHelper('supplier')
+  get: commonGetHelper('supplier', {
+    id: true,
+    vendor: true
+  })
 });
