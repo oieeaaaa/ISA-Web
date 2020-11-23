0bf0793c9cd575db1ad70549898b2a0192f4e311
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-23 19:18:36 +0800

    ✨ New shape: App

---
+++ js/shapes/app.js
@@ -0,0 +1,13 @@
+const defaultValues = {
+  notification: {
+    isActive: false,
+    message: '',
+    duration: 2000,
+    variant: 'success',
+    component: null,
+    close: null,
+    open: null
+  }
+};
+
+export default defaultValues;
