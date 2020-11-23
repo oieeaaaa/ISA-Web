78099d4a2fdee216364ca721bcc8f138f48013b3
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-23 19:17:54 +0800

    ✨ New messages

--- js/messages.js
+++ js/messages.js
@@ -1,9 +1,13 @@
 export default {
   success: {
-    casual: 'Nice!'
+    casual: 'Nice!',
+    add: 'Successfully Added!',
+    update: 'Successfully Updated!'
   },
   error: {
-    casual: 'An error occured!'
+    casual: 'An error occured!',
+    add: 'Failed to add. Please double check your inputs and try again.',
+    update: 'Failed to update. Please double check your inputs and try again.'
   },
   validation: {
     required: 'Required.',
