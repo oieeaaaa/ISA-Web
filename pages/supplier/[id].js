39d39a12da9a654cb8fd17d3e87d8d93be07f52a
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-24 21:44:57 +0800

    ✨ New page: supplier/[id].js

---
+++ pages/supplier/[id].js
@@ -0,0 +1,67 @@
+import { useRouter } from 'next/router';
+import { Formik } from 'formik';
+import messages from 'js/messages';
+import useAppContext from 'js/contexts/app';
+import fetcher from 'js/utils/fetcher';
+import { submitPayload, initialValues } from 'js/shapes/suppliers';
+import validationSchema from 'js/validations/supplier';
+
+// components
+import Layout from 'components/layout/layout';
+import SupplierForm from 'components/supplier-form/supplier-form';
+
+const SupplierItem = ({ item, helpers }) => {
+  const { notification } = useAppContext();
+  const router = useRouter();
+
+  const handleSubmit = async (values, actions) => {
+    try {
+      await fetcher(`/supplier/${router.query.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(submitPayload(values))
+      });
+
+      actions.resetForm();
+      router.push('/supplier');
+      notification.open({
+        variant: 'success',
+        message: messages.success.update
+      });
+    } catch (error) {
+      notification.open({
+        variant: 'danger',
+        message: messages.error.update
+      });
+    }
+  };
+
+  return (
+    <Layout>
+      <Formik
+        initialValues={{
+          ...initialValues,
+          ...item
+        }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}>
+        <SupplierForm mode="edit" helpers={helpers} />
+      </Formik>
+    </Layout>
+  );
+};
+
+export async function getServerSideProps({ params }) {
+  const item = await fetcher(`/supplier/${params.id}`);
+  const brands = await fetcher('/helpers/brand');
+
+  return {
+    props: {
+      item: item.data,
+      helpers: {
+        brands: brands.data
+      }
+    }
+  };
+}
+
+export default SupplierItem;
