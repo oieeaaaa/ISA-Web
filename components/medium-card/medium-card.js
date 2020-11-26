43ba59ac6c50a6b8aa1c206ac56de71ba476ff86
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-26 08:51:22 +0800

    ✨ New component: MediumCard

---
+++ components/medium-card/medium-card.js
@@ -0,0 +1,8 @@
+const MediumCard = ({ title, content }) => (
+  <div className="medium-card">
+    <h3 className="medium-card__title">{title}</h3>
+    <p className="medium-card__content">{content}</p>
+  </div>
+);
+
+export default MediumCard;
