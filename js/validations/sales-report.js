e0c393743b444e4cb0d81df611d785d093c49ea4
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-28 11:00:10 +0800

    🔥 Removed some fields in validationSchema

--- js/validations/sales-report.js
+++ js/validations/sales-report.js
@@ -9,18 +9,5 @@ export default Yup.object().shape({
   }),
   dateCreated: Yup.date().required(message.required),
   name: Yup.string().required(message.required),
-  soldItems: Yup.array().required(message.required),
-
-  tin: Yup.string(),
-  siNumber: Yup.string(),
-  arsNumber: Yup.string(),
-  drNumber: Yup.string(),
-  crsNumber: Yup.string(),
-  address: Yup.string(),
-  chequeNumber: Yup.string(),
-  chequeDate: Yup.date().nullable(),
-  discount: Yup.number(),
-  salesStaff: Yup.array(),
-  paymentType: Yup.object(),
-  bank: Yup.object()
+  soldItems: Yup.array().required(message.required)
 });
