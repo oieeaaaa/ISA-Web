82abb3f64a6c1665d52c56ff9c4c9203e6d23570
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-28 10:02:17 +0800

    🏗  Added initials

--- pages/api/helpers/supplier.js
+++ pages/api/helpers/supplier.js
@@ -4,6 +4,7 @@ import commonGetHelper from 'js/utils/commonGetHelper';
 export default api({
   get: commonGetHelper('supplier', {
     id: true,
-    vendor: true
+    vendor: true,
+    initials: true
   })
 });
