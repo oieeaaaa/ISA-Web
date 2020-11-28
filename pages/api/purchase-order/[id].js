51facfebca62fe90a1d8ff44db55c1c1c3cf04ca
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-28 17:24:11 +0800

    ✨ New api: purchase-order/[id]

---
+++ pages/api/purchase-order/[id].js
@@ -0,0 +1,40 @@
+import { PrismaClient } from '@prisma/client';
+import api from 'js/utils/api';
+
+const prisma = new PrismaClient();
+
+export default api({
+  get: async (req, res) => {
+    try {
+      const item = await prisma.purchaseOrder.findOne({
+        where: {
+          id: req.query.id
+        },
+        include: {
+          supplier: true,
+          items: {
+            include: {
+              item: {
+                include: {
+                  applications: true,
+                  uom: true,
+                  brand: true,
+                  supplier: {
+                    select: {
+                      id: true,
+                      initials: true
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      });
+
+      res.success(item);
+    } catch (error) {
+      res.error(error);
+    }
+  }
+});
