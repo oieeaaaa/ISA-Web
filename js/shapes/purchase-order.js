f577f1d6e1d1a340f285642244b685c70d21fd2d
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-28 16:31:26 +0800

    🏗  Updated shapes

--- js/shapes/purchase-order.js
+++ js/shapes/purchase-order.js
@@ -1,7 +1,5 @@
-import {
-  connectOrCreateMultiple,
-  connectOrCreateSingle
-} from 'js/utils/connectOrCreate';
+import omit from 'lodash.omit';
+import { safeType } from 'js/utils/safety';
 import dateFormat from 'js/utils/dateFormat';
 
 export const tableHeaders = [
@@ -41,31 +39,110 @@ export const tableSortOptions = [
 export const initialValues = {
   dateCreated: new Date(),
   supplier: {},
-  items: []
+  items: [],
+  removedItems: [],
+
+  // Modal related stuff
+  // NOTE: Properties below should not be submitted as a payload
+  modal: {
+    mode: 'add',
+    selectedItem: { particular: '' },
+    selectedQuantity: 0
+  }
 };
 
-export const submitPayload = ({
-  applications,
-  uom,
-  brand,
-  supplier,
-  ...payload
-}) => ({
-  ...payload,
-  applications: connectOrCreateMultiple(applications),
-  brand: connectOrCreateSingle(brand),
-  uom: connectOrCreateSingle(uom),
-  supplier: {
-    connect: {
-      id: supplier.id
-    }
+export const submitPayload = (payload) =>
+  omit(payload, ['modal', 'removedItems']);
+
+export const itemsHeaders = [
+  {
+    label: 'Quantity',
+    accessKey: 'selectedQuantity'
+  },
+  {
+    label: 'Unit of Measurement',
+    accessKey: 'uom.name'
+  },
+  {
+    label: 'Particular',
+    accessKey: 'particular'
+  },
+  {
+    label: 'Parts Number',
+    accessKey: 'partsNumber'
+  },
+  {
+    label: 'Applications',
+    accessKey: 'applications',
+    customCell: ({ value }) =>
+      safeType
+        .array(value)
+        .map((val) => val.name)
+        .join(', ')
+  },
+  {
+    label: 'Description',
+    accessKey: 'description'
+  },
+  {
+    label: 'Size',
+    accessKey: 'size'
+  },
+  {
+    label: 'Codes',
+    accessKey: 'codes'
+  },
+  {
+    label: 'Remarks',
+    accessKey: 'remarks'
   }
-});
+];
+
+export const itemsSortOptions = [
+  {
+    name: 'Quantity',
+    key: 'quantity'
+  },
+  {
+    name: 'Unit of Measurement',
+    key: 'uom.name'
+  },
+  {
+    name: 'Particular',
+    key: 'particular'
+  },
+  {
+    name: 'Parts Number',
+    key: 'partsNumber'
+  },
+  {
+    name: 'Applications',
+    key: 'applications'
+  },
+  {
+    name: 'Description',
+    key: 'description'
+  },
+  {
+    name: 'Size',
+    key: 'size'
+  },
+  {
+    name: 'Codes',
+    key: 'codes'
+  },
+  {
+    name: 'Remarks',
+    key: 'remarks'
+  }
+];
 
 export default {
   initialValues,
   submitPayload,
   tableHeaders,
   tableSortOptions,
-  tableFilters
+  tableFilters,
+  itemsHeaders,
+  itemsSortOptions
 };
