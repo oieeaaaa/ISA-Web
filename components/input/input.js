5ab517065899c9cbd23b2cba33403bf779026fdf
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-29 14:32:18 +0800

    💅 Allowed new className

--- components/input/input.js
+++ components/input/input.js
@@ -1,5 +1,8 @@
 import { Field } from 'formik';
+import joinClassName from 'js/utils/joinClassName';
 
-const Input = ({ ...etc }) => <Field className="input" type="text" {...etc} />;
+const Input = ({ className, ...etc }) => (
+  <Field className={joinClassName('input', className)} type="text" {...etc} />
+);
 
 export default Input;
