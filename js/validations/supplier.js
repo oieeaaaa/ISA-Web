2cbd2930fee945f0158cab9f9d46af1b5bc57921
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-29 19:01:29 +0800

    🔥 Removed unnecessary validation

--- js/validations/supplier.js
+++ js/validations/supplier.js
@@ -8,15 +8,5 @@ export default Yup.object().shape({
   vendor: Yup.string().required(message.required),
   entry: Yup.date().required(message.required),
   initials: Yup.string().required(message.required),
-  terms: Yup.number().min(0).required(message.required),
-
-  // optional
-  owner: Yup.string(),
-  tin: Yup.string(),
-  representative: Yup.string(),
-  address: Yup.string(),
-  brands: Yup.array(),
-  companyPhoneNumbers: Yup.array(),
-  representativePhoneNumbers: Yup.array(),
-  emails: Yup.array()
+  terms: Yup.number().min(0).required(message.required)
 });
