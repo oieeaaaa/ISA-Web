114ddd33615da6a1753821294b973287af5b6a91
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-29 19:01:14 +0800

    🏗  Supported multiple disconnect

--- js/utils/disconnect.js
+++ js/utils/disconnect.js
@@ -1,5 +1,21 @@
+import { safeType } from './safety';
+
 export const disconnectSingle = (data) => (data ? { disconnect: true } : {});
 
+export const disconnectMultiple = (data) => {
+  const disconnectList = safeType
+    .array(data)
+    .map(({ id }) => ({ id: safeType.string(id) }))
+    .filter((d) => !!d.id);
+
+  if (!disconnectList.length) return {};
+
+  return {
+    disconnect: disconnectList
+  };
+};
+
 export default {
-  disconnectSingle
+  disconnectSingle,
+  disconnectMultiple
 };
