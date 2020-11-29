bf706ac0cbfa948b3532727fc4f7f2bd93306d10
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-11-29 14:32:42 +0800

    🏗  Enhanced multi-input

--- components/multi-input/multi-input.js
+++ components/multi-input/multi-input.js
@@ -1,58 +1,73 @@
 import { useEffect } from 'react';
 import { useField } from 'formik';
-import set from 'lodash.set';
 import { safeType } from 'js/utils/safety';
 import Button from 'components/button/button';
 
-const MultiInput = ({ name, mainKey, ...etc }) => {
+// TODO: Replace the custom input with Input component
+const MultiInput = ({
+  name,
+  mainKey = 'name',
+  customInput: CustomInput,
+  ...etc
+}) => {
   const [field, , helpers] = useField({
     name,
     multiple: true
   });
 
   const addField = () => {
-    const newValue = field.value.concat({ [mainKey]: null, isNew: true });
-    helpers.setValue(newValue);
-  };
-
-  const removeField = (index) => {
-    helpers.setValue(field.value.filter((_, valIndex) => valIndex !== index));
-  };
-
-  const handleChange = (index) => (e) => {
-    field.value[index] = set(
-      safeType.object(field.value[index]),
-      mainKey,
-      e.target.value
+    helpers.setValue(
+      field.value.concat({
+        [mainKey]: '',
+        isNew: true
+      })
     );
+  };
 
-    helpers.setValue(field.value);
+  const removeField = (uniqueVal) => {
+    helpers.setValue(field.value.filter((v) => v[mainKey] !== uniqueVal));
+  };
+
+  const handleChange = (uniqueVal) => {
+    helpers.setValue(
+      field.value.map((v) =>
+        v[mainKey] === uniqueVal ? { ...v, [mainKey]: uniqueVal } : v
+      )
+    );
   };
 
   useEffect(() => {
     if (field.value.length) return;
 
-    helpers.setValue([{ [mainKey]: null, isNew: true }]);
+    helpers.setValue([{ [mainKey]: '', isNew: true }]);
   }, [field.value]);
 
   return (
     <div className="multi-input">
       <ul className="multi-input__list">
         {field.value.map((value, index) => (
-          <li key={index} className="multi-input__item">
-            <input
-              className="multi-input__input"
-              type="text"
-              value={safeType.string(value[mainKey])}
-              onChange={handleChange(index)}
-              onBlur={helpers.onBlur}
-              {...etc}
-            />
+          <li key={value[mainKey]} className="multi-input__item">
+            {CustomInput ? (
+              <CustomInput
+                name={`${name}[${index}]`}
+                value={value}
+                onChange={helpers.setValue}
+              />
+            ) : (
+              <input
+                className="multi-input__input"
+                type="text"
+                value={safeType.string(value[mainKey])}
+                onChange={(e) => handleChange(e.target.value)}
+                onBlur={helpers.onBlur}
+                {...etc}
+              />
+            )}
             {index !== 0 && (
               <Button
                 className="multi-input__close"
                 icon="x"
-                onClick={() => removeField(index)}
+                onClick={() => removeField(value[mainKey])}
               />
             )}
           </li>
