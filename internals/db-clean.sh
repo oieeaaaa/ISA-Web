47c6cface6a221335136f043f19ed4ed086d47fb
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-12-02 07:00:10 +0800

    🔥 Changed database

--- internals/db-clean.sh
+++ internals/db-clean.sh
@@ -1,9 +1,3 @@
-rm -rf prisma/migrations # Wipe-out migrations history
-rm prisma/dev.db # Wipe-out database
-touch prisma/dev.db # Create a new one
-
-echo "DB is clean ✨"
-
 yarn prisma:update
 
 echo "Success! ✅"
