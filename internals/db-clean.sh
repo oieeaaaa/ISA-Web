ab1400a145d59c672d53fa93de92c5e1f61c3dc4
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-12-03 22:12:43 +0800

    🔥 Updated db-clean.sh

--- internals/db-clean.sh
+++ internals/db-clean.sh
@@ -1,3 +1,12 @@
-yarn prisma:update
-
-echo "Success! ✅"
+while true; do
+    read -p "WARNING: This will wipe out the entire database, do you wish to proceed?" yn
+    case $yn in
+        [Yy]* )
+            dropdb 'ask';
+            create db 'ask';
+            yarn prisma:update;
+          break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
