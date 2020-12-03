2bbfcfb219b045491a7ae727b5c5bab9d8dcd7c4
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-12-03 22:12:09 +0800

    ✨ New shape: stock-in

---
+++ js/shapes/stock-in.js
@@ -0,0 +1,12 @@
+export const stockInAttributes = {
+  dateCreated: true,
+  referenceNumber: true,
+  referenceDate: true,
+  receivedBy: true,
+  checkedBy: true,
+  codedBy: true,
+  remarks: true,
+  items: true,
+  supplier: true,
+  supplierID: true
+};
