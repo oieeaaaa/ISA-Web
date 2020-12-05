b1cde33a33d45844b7b899e52330cc372e4a3ca3
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-12-05 17:43:19 +0800

    🏗  Allowed custom class

--- components/form-section-title/form-section-title.js
+++ components/form-section-title/form-section-title.js
@@ -1,5 +1,7 @@
-const FormSectionTitle = ({ title }) => (
-  <h3 className="form-section-title">{title}</h3>
+import joinClassName from 'js/utils/joinClassName';
+
+const FormSectionTitle = ({ className, title }) => (
+  <h3 className={joinClassName('form-section-title', className)}>{title}</h3>
 );
 
 export default FormSectionTitle;
