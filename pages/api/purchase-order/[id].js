12bb30b169798d585f2acf7d7a5ec030be338072
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-12-11 21:54:18 +0800

    🏗  Refactored: purchase-order/[id] route

--- pages/api/purchase-order/[id].js
+++ pages/api/purchase-order/[id].js
@@ -1,28 +1,32 @@
 import prisma from 'prisma-client';
+import { select } from 'js/shapes/prisma-query';
+import { variantAttributes } from 'js/shapes/variant';
+import safety from 'js/utils/safety';
 import api from 'js/utils/api';
+import codeCalc from 'js/utils/codeCalc';
 
 export default api({
   get: async (req, res) => {
     try {
-      const item = await prisma.purchaseOrder.findOne({
+      let po = await prisma.purchaseOrder.findOne({
         where: {
           id: req.query.id
         },
         include: {
           supplier: true,
+          tracking: true,
           items: {
             include: {
               item: {
-                include: {
-                  applications: true,
-                  uom: true,
-                  brand: true,
-                  supplier: {
-                    select: {
-                      id: true,
-                      initials: true
-                    }
-                  }
+                select: {
+                  ...variantAttributes,
+                  supplier: select(['vendor', 'initials']),
+                  inventory: select([
+                    'id',
+                    'particular',
+                    'partsNumber',
+                    'quantity'
+                  ])
                 }
               }
             }
@@ -30,7 +34,20 @@ export default api({
         }
       });
 
-      res.success(item);
+      const codes = await prisma.code.findMany({});
+
+      po = {
+        ...po,
+        items: po.items.map((item) => ({
+          ...item,
+          item: {
+            ...item.item,
+            unitCost: codeCalc(codes, safety(item, 'item.codes', ''))
+          }
+        }))
+      };
+
+      res.success(po);
     } catch (error) {
       res.error(error);
     }
