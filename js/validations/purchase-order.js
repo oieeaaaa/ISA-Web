2efdd7865642a53e339f22754b13f3d6c7761a5f
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-12-11 21:52:05 +0800

    🏗  Refactored: updated purchase-order validation

--- js/validations/purchase-order.js
+++ js/validations/purchase-order.js
@@ -4,9 +4,15 @@ import messages from 'js/messages';
 const { validation: message } = messages;
 
 export default Yup.object().shape({
-  supplier: Yup.object().shape({
-    id: Yup.string().required(message.required)
-  }),
-  dateCreated: Yup.date().required(message.required),
+  supplier: Yup.object()
+    .shape({
+      id: Yup.string().required(message.required)
+    })
+    .required(message.required),
+  tracking: Yup.object()
+    .shape({
+      address: Yup.string().required(message.required)
+    })
+    .required(message.required),
   items: Yup.array().required(message.required)
 });
