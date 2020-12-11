2a9ea54ce2b5ae751717d13a4742c73d4af49eaf
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-12-11 21:52:20 +0800

    🏗  Refactored: updated sales-report validation

--- js/validations/sales-report.js
+++ js/validations/sales-report.js
@@ -4,10 +4,11 @@ import messages from 'js/messages';
 const { validation: message } = messages;
 
 export default Yup.object().shape({
-  type: Yup.object().shape({
-    name: Yup.string().required(message.required)
-  }),
-  dateCreated: Yup.date().required(message.required),
+  type: Yup.object()
+    .shape({
+      name: Yup.string().required(message.required)
+    })
+    .required(message.required),
   name: Yup.string().required(message.required),
-  soldItems: Yup.array().required(message.required)
+  items: Yup.array().required(message.required)
 });
