443da8252356d92cec6890bc25e0876dc222e411
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-12-16 20:59:59 +0800

    🏗  Updated verifyLogin

--- js/utils/verifyLogin.js
+++ js/utils/verifyLogin.js
@@ -1,10 +1,11 @@
+import { AUTH_KEY } from 'js/shapes/cookies';
 import parseCookies from './parseCookies';
 import safety from './safety';
 
 const verifyLogin = (req, res) => {
   const data = parseCookies(req);
 
-  if (!safety(data, 'user', '')) {
+  if (!safety(data, AUTH_KEY, '')) {
     res.writeHead(301, { Location: '/login' });
     res.end();
 
