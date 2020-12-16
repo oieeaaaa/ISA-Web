223e99c8e0beb92f4d5b3049922472f479cde959
Joimee Tan Cajandab <joimee.cajandab@gmail.com>
2020-12-16 20:59:37 +0800

    🏗  Updated header shapes

--- js/shapes/header.js
+++ js/shapes/header.js
@@ -1,21 +1,35 @@
+import { initialValues as profileInitialValues } from 'js/shapes/profile';
+import { initialValues as settingsInitialValues } from 'js/shapes/settings';
+import { initialValues as changePasswordInitialValues } from 'js/shapes/change-password';
+
+import profileValidation from 'js/validations/profile';
+import settingsValidation from 'js/validations/settings';
+import changePasswordValidation from 'js/validations/change-password';
+
 import Settings from 'components/settings/settings';
 import Profile from 'components/profile/profile';
-
-export const initialValues = {
-  codes: [],
-  removedCodes: [],
-  user: {}
-};
+import ChangePassword from 'components/change-password/change-password';
 
 export const menus = [
   {
     key: 'profile',
     label: 'Profile',
-    modal: Profile
+    modal: Profile,
+    initialValues: profileInitialValues,
+    validationSchema: profileValidation
+  },
+  {
+    key: 'change-password',
+    label: 'Change Password',
+    modal: ChangePassword,
+    initialValues: changePasswordInitialValues,
+    validationSchema: changePasswordValidation
   },
   {
     key: 'settings',
     label: 'Settings',
-    modal: Settings
+    modal: Settings,
+    initialValues: settingsInitialValues,
+    validationSchema: settingsValidation
   }
 ];
